2896a8c2ad2e6cebef0db63a1408e082f178219d
smkumatela <smkumatela@webmail.co.za>
2016-06-06 11:34:45 +0200

    serviceAgain

--- app/src/main/java/com/example/songezo/assignment6_android/activities/DB_View_Activity.java
+++ app/src/main/java/com/example/songezo/assignment6_android/activities/DB_View_Activity.java
@@ -1,7 +1,10 @@
 package com.example.songezo.assignment6_android.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import com.example.songezo.assignment6_android.R;
 
@@ -11,5 +14,13 @@ public class DB_View_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_db__view_);
+    }
+
+    public void onButtonClick(View v){
+        if (v.getId() == R.id.bHome){
+            Intent i = new Intent(DB_View_Activity.this, MainActivity_Login.class);
+
+            startActivity(i);
+        }
     }
 }
